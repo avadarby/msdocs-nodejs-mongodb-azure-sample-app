2f83667d8e0ced88c7dac9793eaa7aa40e274de7
avadarby <130720186+avadarby@users.noreply.github.com>
2023-04-14 13:18:50 +1000

    console change 2

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,8 @@ router.get('/', function(req, res, next) {
       const currentTasks = tasks.filter(task => !task.completed);
       const completedTasks = tasks.filter(task => task.completed === true);
 
-      console.log(`Total tasks: ${tasks.length}   Current tasks: ${currentTasks.length}    Completed tasks:  ${completedTasks.length}`)
       console.log('heyyy');
+      console.log(`Total tasks: ${tasks.length}   Current tasks: ${currentTasks.length}    Completed tasks:  ${completedTasks.length}`)
       res.render('index', { currentTasks: currentTasks, completedTasks: completedTasks });
       
     })

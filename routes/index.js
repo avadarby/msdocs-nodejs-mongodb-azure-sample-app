4c2605adb762f3671fec722f02e6d06e2a587f5e
avadarby <130720186+avadarby@users.noreply.github.com>
2023-04-14 13:32:18 +1000

    cons change 7

--- routes/index.js
+++ routes/index.js
@@ -10,11 +10,10 @@ router.get('/', function(req, res, next) {
       const currentTasks = tasks.filter(task => !task.completed);
       const completedTasks = tasks.filter(task => task.completed === true);
 
-      console.log('heyyy');
-      res.render('index');
+      console.log("heyyyy");
 
       console.log(`Total tasks: ${tasks.length}   Current tasks: ${currentTasks.length}    Completed tasks:  ${completedTasks.length}`)
-      res.render('heyyyyy', 'index', { currentTasks: currentTasks, completedTasks: completedTasks });
+      res.render('index', { currentTasks: currentTasks, completedTasks: completedTasks });
     })
     .catch((err) => {
       console.log(err);
@@ -32,7 +31,6 @@ router.post('/addTask', function(req, res, next) {
     createDate: createDate
   });
   console.log(`Adding a new task ${taskName} - createDate ${createDate}`)
-  console.log('heyyy')
 
   task.save()
       .then(() => { 
